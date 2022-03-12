02fc967ab125b0dc2bff3ef3cd78c7e45e5a2b57
emmanuelalmonte <ammetronics67@gmail.com>
2022-03-11 19:19:32 -0500

    initializing .gitignore for API key

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID YM4O9gy0RlC8xV5DwLvFLCEDWI6ApDYBbqyn4BvZSLA' 
-  } 
-})
